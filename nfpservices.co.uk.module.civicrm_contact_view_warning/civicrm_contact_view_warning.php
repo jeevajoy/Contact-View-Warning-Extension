3bbbb799915c724e4e10f3b6dab9c8b54caf685a
jeevajoy <jag.kandasamy@gmail.com>
2012-10-15 14:04:36 +0100

    Updated 15 Oct 2012

--- nfpservices.co.uk.module.civicrm_contact_view_warning/civicrm_contact_view_warning.php
+++ nfpservices.co.uk.module.civicrm_contact_view_warning/civicrm_contact_view_warning.php
@@ -51,6 +51,20 @@ function civicrm_contact_view_warning_civicrm_config( &$config ) {
     set_include_path( $include_path );
 }
 
+
+function civicrm_contact_view_warning_civicrm_install(){
+    $query = "CREATE TABLE IF NOT EXISTS `mtl_civicrm_note_is_warning` (
+             `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Default MySQL primary key',
+             `entity_id` int(10) unsigned NOT NULL COMMENT 'Note Id',
+             `contact_id` int(10) unsigned NOT NULL COMMENT 'Contact Id',
+             `is_warning` int(10) unsigned NOT NULL COMMENT 'Is the Contribution Type Gift-Aidable?',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `unique_entity_id` (`entity_id`)
+              ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+
+   CRM_Core_DAO::executeQuery($sql);
+}
+
 /*
  * Implementation of hook_civicrm_tokens 
  */ 
